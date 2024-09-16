0a7f0ad84486ce4c3fc76fed3331856efade6710
Brian Lee <briandl92391@gmail.com>
2024-09-15 16:47:45 -0800

    circleshape

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def main():
 
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+	game_clock = pygame.time.Clock()
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
